cb49630ec85849e423f5c73983855eed859ba020
Jose Lucas Costa <joselucascosta@MacBook-Air-de-Jose.local>
2023-09-15 18:31:51 -0400

    Corrigindo pag principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <header className="flex justify-between p-2 px-12 items-center  ">
         <Link href='/' className="text-center leading-3 hover:text-red-950 ease-in-out duration-200 hover:font-semibold">Moyses<br/>Israel</Link>
         <div className="flex item-center justify-center max-sm:hidden">
-          <Link href='/' className="px-5 text-slate-800 hover:text-slate-400 hover:underline self-center"><p className="">Inicio</p></Link>
+          <Link href='/' className="px-5 text-slate-800 text-slate-400 self-center drop-shadow-lg"><p className="">Inicio</p></Link>
           <Link href='/' className="px-5 text-slate-800 hover:text-slate-400 hover:underline self-center">Memorial</Link>
           <Link href='/' className="px-5 text-slate-800 hover:text-slate-400 hover:underline self-center">Biografia</Link>
           <Link href='/' className="px-5 text-slate-800 hover:text-slate-400 hover:underline self-center">Contato</Link>
